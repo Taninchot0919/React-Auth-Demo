bebf03a6c7a78d8f229f50d18e2045d34853e9f9
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-06-08 12:46:18 +0700

    [ADD] First init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Signup from "./Signup";
+
+function App() {
+  return (
+    <Container>
+      <Signup />
+    </Container>
+  );
+}
+
+export default App;
